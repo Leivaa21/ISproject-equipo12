e6c90d2954b24dffbe9834c097e6dabb58aebf7d
Lucena8lf <71568195+Lucena8lf@users.noreply.github.com>
2020-12-19 15:34:44 +0100

    Makefile y primer test

---
+++ ruta_unittest.cc
@@ -0,0 +1,28 @@
+//ruta_unittest.cc
+//A program that tests some Ruta's methods
+
+#include <list>
+#include <string>
+#include "ruta.h"
+#include "sendero.h"
+#include "parque.h"
+#include "gtest/gtest.h"
+
+
+using namespace std;
+
+TEST(Ruta, addParticipante){
+
+	Parque Cordoba = Parque("Cordoba",5000,"Cordoba","Cordoba","Cordoba","07-10-95","");
+	Sendero Nevado = Sendero("Nevado", "Clima nevado", "Media", "a", Cordoba);
+	//Ruta rut = Ruta(00,7,"Ninguno",45,"17-05-2021, 19:00", Nevado,15);
+	Ruta rut = Ruta();
+
+	Visitante v1 = Visitante("Antonio","Fernandez","Fernandez","52819202G","12-02-01",681280132);
+	
+	EXPECT_TRUE(rut.addParticipante(v1));
+	EXPECT_EQ(1,rut.getParticipantes().size());
+	EXPECT_EQ("52819202G", rut.getParticipantes().begin()->getDni());
+	EXPECT_EQ(681280132, rut.getParticipantes().begin()->getTelefono());
+
+}
